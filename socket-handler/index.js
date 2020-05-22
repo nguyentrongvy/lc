53fb6c63deaa138b45146a31f551f122a00287bd
duy.nguyen <duy.nguyen@vnlp.ai>
2020-05-22 07:28:05 +0000

    VNLP-637 - update load live chat rely on bot

--- socket-handler/index.js
+++ socket-handler/index.js
@@ -1,16 +1,26 @@
+const _ = require('lodash');
 const { initialize } = require('./initialize-socket');
 const jwtHelper = require('../helpers/jwt.helper');
 const Constants = require('../common/constants');
+const { verifyBotId } = require('../middlewares/authentication.middleware');
 
-function authenticationUser(socket, next) {
+async function authenticationUser(socket, next) {
 	try {
-		const { token } = socket.handshake.query;
+		const { token, botId } = socket.handshake.query;
 		if (!token) {
 			return next(new Error(Constants.ERROR.INVALID_TOKEN));
 		}
 		const verifiedData = jwtHelper.verifyToken(token);
+		const org = _.get(verifiedData, 'org._id');
+		const isVerified = await verifyBotId(org, botId);
+		if (!isVerified) {
+			throw new Error('Bot is not belong to organization');
+		}
+
 		socket.user = verifiedData.user;
-		socket.engine = verifiedData.engine;
+		socket.engine = {
+			_id: botId,
+		};
 		return next();
 	} catch (error) {
 		console.error(error);
