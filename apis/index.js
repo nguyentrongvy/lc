fc59ab10addd06ad42b45cc57b554d6e7b654f31
phuong tran <phuong.tran@emandai.net>
2021-06-22 08:46:13 +0000

    VNLP-3558 Update load apis

--- apis/index.js
+++ apis/index.js
@@ -1,9 +1,9 @@
 exports.load = (app) => {
-	require('./broadcast-message.api');
-	require('./broadcast-response.api');
-	require('./message');
-	require('./notification');
-	require('./room');
-	require('./tag');
-	require('./timer.api');
+	require('./broadcast-message.api').load(app);
+	require('./broadcast-response.api').load(app);
+	require('./message').load(app);
+	require('./notification').load(app);
+	require('./room').load(app);
+	require('./tag').load(app);
+	require('./timer.api').load(app);
 };
